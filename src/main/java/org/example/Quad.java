3de8ab7f4479ebc6c2d568e06f33bcd460bf4f5c
Vsevolod <pro.vsevolod64@gmail.com>
2024-05-19 19:55:16 +0200

    Quad: Add side + constructor + print method

--- src/main/java/org/example/Quad.java
+++ src/main/java/org/example/Quad.java
@@ -1,4 +1,53 @@
 package org.example;
 
-public class Quad {
+public class Quad extends  Shape {
+     private int side;
+
+     public Quad(int side) {
+         if (side <= 0) {
+             throw new IllegalArgumentException("Side cannot be <= 0");
+         }
+         this.side = side;
+     }
+
+     @Override
+    public void print() {
+
+         try {
+             if (side > 100) {
+                 throw new RuntimeException("Side is too big! I cannot draw it here!");
+             }
+         } catch (RuntimeException ex) {
+             System.out.println(ex.getMessage());
+             return;
+         }
+
+         System.out.println("Shape: Quad");
+         System.out.println("Side length: " + side);
+
+         if (side == 1) {
+             System.out.print("*");
+             return;
+         }
+
+         for (int i = 0; i < side; i++) {
+             System.out.print("* ");
+         }
+         System.out.println();
+
+         for (int j = 0; j < side-2; j++) {
+             System.out.print("* ");
+             for (int i = 0; i < side-2; i++) {
+                 System.out.print("  ");
+             }
+             System.out.println("*");
+         }
+
+         for (int i = 0; i < side; i++) {
+             System.out.print("* ");
+         }
+     }
+
+     public int getSide() { return side; }
+
 }
