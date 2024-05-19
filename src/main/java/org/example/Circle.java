b0893ff0cffdc2502f2102396f018c4e3e9f6a5b
Vsevolod <pro.vsevolod64@gmail.com>
2024-05-19 12:35:01 +0200

    Circle: Add constructor + field radius + getter

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -1,5 +1,18 @@
 package org.example;
 
-public class Circle {
+public class Circle extends Shape {
+    private final int radius;
 
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    public int getRadius() {
+        return radius;
+    }
 }
