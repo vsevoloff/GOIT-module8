9988d61f5d2f8fa49b68114507e7da5e255cc7ab
Vsevolod <pro.vsevolod64@gmail.com>
2024-05-19 14:11:13 +0200

    Circle: Add 'print' method + private 'coordinate' method

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.nio.file.Path;
+
 public class Circle extends Shape {
     private final int radius;
 
@@ -10,9 +12,35 @@ public class Circle extends Shape {
     @Override
     public void print() {
 
+        System.out.println("Shape: circle");
+        System.out.println("Radius: " + radius);
+
+        // draw circle
+        int width = radius;
+        int length = (int) (width * 1.5);
+        int y = width;
+        while (y >= - width) {
+            int x = - length;
+            while (x <= length) {
+                if(Circle.coordinate(x,y) == radius){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+                x++;
+            }
+            System.out.println();
+            y -= 2;
+        }
     }
 
-    public int getRadius() {
+    private static int coordinate(int x, int y) {
+        return (int) (Math.sqrt(Math.pow(x,2) + Math.pow(y,2)));
+    }
+
+
+    public int getRadius () {
         return radius;
     }
+
 }
