a2a83f488b468db4d9eafa9c445acd4ed69293e5
Vsevolod <pro.vsevolod64@gmail.com>
2024-05-21 17:14:31 +0200

    Triangle: fixed bugs

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,4 +1,41 @@
 package org.example;
 
-public class Triangle {
+public class Triangle extends  Shape{
+    public int side;
+
+    public Triangle(int side) {
+        if (side <= 0) {
+            throw  new IllegalArgumentException("Side cannot be <= 0");
+        }
+        this.side = side;
+    }
+
+    @Override
+    public void print() {
+
+        System.out.println("Shape: Triangle");
+        System.out.println("Side length:" + side);
+
+        try {
+            if (side > 100) {
+                throw new RuntimeException("Side is too big! I cannot draw it here!");
+            }
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        for (int i = 0; i <= side; i++) {
+            for (int j = 0; j <= side * 2; j++) {
+                if (j == side - i || j == side + i || i == side) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public int getSide() { return side; }
 }
