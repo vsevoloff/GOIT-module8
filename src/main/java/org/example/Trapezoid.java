f83325b9d5720d05e1820a992bfdfddb5c9a6337
Vsevolod <pro.vsevolod64@gmail.com>
2024-05-26 12:15:55 +0200

    Trapezoid: Add exceptions

--- src/main/java/org/example/Trapezoid.java
+++ src/main/java/org/example/Trapezoid.java
@@ -6,6 +6,15 @@ public class Trapezoid  extends  Shape{
      private int basis2;
 
      public Trapezoid(int height,int basis,int basis2) {
+
+        try {
+            if (height <= 0 || basis <= 0 || basis2 <= 0) {
+                throw new IllegalArgumentException("One of the arguments is less than or equal to zero!");
+            }
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
           this.height = height;
           this.basis = basis;
           this.basis2 = basis2;
@@ -13,6 +22,20 @@ public class Trapezoid  extends  Shape{
 
      @Override
      public void print() {
+
+         System.out.println("Shape: Trapezoid");
+         System.out.println("Height" + height);
+         System.out.println("Basis: " + basis);
+         System.out.println("Second basis: " + basis2);
+
+         try {
+             if (height > 100 || basis > 100 || basis2 > 100) {
+                 throw new RuntimeException("One of the sides is too big. I cannot draw it!");
+             }
+         } catch (RuntimeException exception) {
+             System.out.println(exception.getMessage());
+             return;
+         }
 
           int difference = basis2 - basis;
           double step = (double) difference / height;
